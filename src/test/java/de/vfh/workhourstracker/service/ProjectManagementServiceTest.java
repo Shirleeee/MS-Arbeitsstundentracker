dfe5524d57492bb90deb14f2bc539237de27a562
Josephine <74024200+joho1910@users.noreply.github.com>
2024-11-10 12:07:41 +0100

    Update ProjectManagementServiceTest.java

--- src/test/java/de/vfh/workhourstracker/service/ProjectManagementServiceTest.java
+++ src/test/java/de/vfh/workhourstracker/service/ProjectManagementServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.LocalDate;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class ProjectManagementServiceTest {
 
     @Autowired
